fb4da30f67fce6cf840fe5bcf7224c6c352562c8
Atharva123987 <atharva.amberkar@gmail.com>
2023-04-20 00:44:46 +0530

    Dashboard responsiveness done

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -40,6 +40,7 @@ const dashboard = () => {
           const response = await axios.get(
             `https://api.twelvedata.com/time_series?symbol=AMZN&interval=1week&apikey=${process.env.NEXT_PUBLIC_TWELVE_API_KEY}`
           );
+          console.log(response)
           const series = response?.data?.values?.map((value) => ({
             name: value.datetime,
             "Close Price": value.close,
