11f10392359dfcc031e0a138f86dd40ef60feae1
Atharva123987 <atharva.amberkar@gmail.com>
2023-04-20 12:00:13 +0530

    Skeleton loaders for async components

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -12,6 +12,8 @@ import { UilUsersAlt } from '@iconscout/react-unicons'
 import LChart from '../components/LChart';
 import PChart from '../components/PChart';
 import axios from 'axios'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 const dashboard = () => {
     const { data: session } = useSession();
     const router = useRouter()
@@ -64,7 +66,146 @@ const dashboard = () => {
         signOut({ callbackUrl: "/" }); // Redirect to the home page after signing out
     };
 
+    if (!data || !data2 || !data1) {
+        return (<>
 
+            <div className="container" id='dashboard-container' >
+
+                <div id="sidebar">
+                    <h2>Board.</h2>
+
+                    <div id='sidebar-links'>
+                        <a><AiOutlinePieChart /> Dashboard</a>
+                        <a><BsTags /> Transactions</a>
+                        <a><TbCalendarTime /> Schedules</a>
+                        <a><BiUserCircle /> Users</a>
+                        <a><SlSettings /> Settings</a>
+                    </div>
+
+                    <div id='sidebar-footer'>
+                        <button onClick={handleSignOut}><CgLogOut /> Log Out</button>
+                        <a>Help</a>
+                        <a>Contact Us</a>
+                    </div>
+
+                </div>
+
+                <div id="dashboard">
+
+                    <div id="navbar">
+
+                        <h1>Dashboard </h1>
+
+                        <div id='navbar-right'>
+                            <AiOutlineSearch id='search-icon' />
+                            <input type='text' placeholder='Search...' />
+                            <BiBell />
+                            <img id='profile-image' src='https://xsgames.co/randomusers/avatar.php?g=male' alt='profile-image' />
+                        </div>
+
+                    </div>
+
+                    <div id='grid-container'>
+
+                        <div className='top-cards'>
+                            <div>
+                                <p>Total revenues</p>
+                                <h3>$2,129,430</h3>
+                            </div>
+                            <UilMoneyWithdraw />
+                        </div>
+
+                        <div className='top-cards'>
+                            <div>
+                                <p>Total Transactions</p>
+                                <h3>1,520</h3>
+                            </div>
+                            <BsTags />
+                        </div>
+
+                        <div className='top-cards'>
+                            <div>
+                                <p>Total Likes</p>
+                                <h3>9,721</h3>
+                            </div>
+                            <AiOutlineLike />
+                        </div>
+
+                        <div className='top-cards'>
+                            <div>
+                                <p>Total Users</p>
+                                <h3>892</h3>
+                            </div>
+                            <UilUsersAlt />
+                        </div>
+
+
+                        <div className='grid-span-4' id='line-chart'>
+                            <div>
+                                <h3>Activities</h3>
+                                <select>
+                                    {options.map((option, index) => (
+                                        <option key={index} value={option}>
+                                            {option}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                            <Skeleton count={1} width={1000} />
+                            <Skeleton count={1} height={100} width={1000} highlightColor="#D3D3D3" />
+                            <Skeleton count={1} width={1000} />
+                        </div>
+
+                        <div className='grid-span-2 schedule-card' id='pie-chart'>
+                            <div className='schedule-top'>
+                                <h3>Top products</h3>
+                                <select>
+                                    {options.map((option, index) => (
+                                        <option key={index} value={option}>
+                                            {option}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                            <div style={{display:"flex", justifyContent:"space-between"}}>
+                            <Skeleton count={1} width={150} height={150} highlightColor="#D3D3D3" style={{ borderRadius: "50%", marginLeft: "4rem", marginTop: "1rem" }} />
+                            <div>
+                            <Skeleton count={1} width={100} style={{marginTop:"4rem", marginRight:"4rem"}}/>
+                            <Skeleton count={1} width={100} style={{marginTop:"0.5rem", marginRight:"4rem"}}/>
+                            <Skeleton count={1} width={100} style={{marginTop:"0.5rem", marginRight:"4rem"}}/>
+                            </div>
+                            </div>
+                        </div>
+
+                        <div className='grid-span-2 schedule-card'>
+
+                            <div className='schedule-top'>
+                                <h3>Today's Schedule</h3>
+                                <p>See all<BiChevronRight /></p>
+                            </div>
+
+                            <div id='schedule-1'>
+                                <h5>Meeting with suppliers from Kuta Bali</h5>
+                                <p>14.00-15.00</p>
+                                <p>at Sunset Road, Kuta, Bali </p>
+                            </div>
+
+                            <div id='schedule-2'>
+                                <h5>Check operation at Giga Factory 1</h5>
+                                <p>18.00-20.00</p>
+                                <p>at Central Jakarta </p>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </>
+        )
+    }
 
     return (
         <>
